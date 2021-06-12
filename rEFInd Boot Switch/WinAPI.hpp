ad639e0c125a86f8edc0d7b5e0e199c9b58fd20f
PlayDay <18056374+playday3008@users.noreply.github.com>
2021-06-12 21:37:31 +0200

    Update WinAPI.hpp

--- rEFInd Boot Switch/WinAPI.hpp
+++ rEFInd Boot Switch/WinAPI.hpp
@@ -55,8 +55,49 @@ inline auto UpdatePrevBootVar(const std::string lpName, const std::string lpGuid
     return true;
 }
 
+inline auto SetShutdownPrivilege()
+{
+    TOKEN_PRIVILEGES tp{};
+    LUID luid{};
+    HANDLE hToken = nullptr;
+
+    if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken)) {
+        std::cout << "Can't open process token" << std::endl;
+        return false;
+    }
+
+    if (!LookupPrivilegeValue(nullptr, SE_SHUTDOWN_NAME, &luid)) {
+        std::cout << "Can't lookup SE_SHUTDOWN_NAME privilege" << std::endl;
+        return false;
+    }
+
+    tp.PrivilegeCount = 1;
+    tp.Privileges[0].Luid = luid;
+    tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+
+    if (!AdjustTokenPrivileges(hToken, false, &tp, sizeof(TOKEN_PRIVILEGES), nullptr, nullptr)) {
+        std::cout << "Can't adjust token privileges" << std::endl;
+        return false;
+    }
+
+    if (!CloseHandle(hToken)) {
+        std::cout << "Can't close token handle" << std::endl;
+        return false;
+    }
+
+    if (GetLastError() == ERROR_NOT_ALL_ASSIGNED) {
+        std::cout << "Can't not all privileges assigned" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
 inline auto Reboot()
 {
+    if (!SetShutdownPrivilege())
+        return false;
+	
     if (!ExitWindowsEx(EWX_REBOOT,
         SHTDN_REASON_MAJOR_OTHER |
         SHTDN_REASON_MINOR_OTHER |
